33f874e7101f9d2951f6010caee3b719fdb17a9a
ckazu <ckazu.s@gmail.com>
2017-05-07 18:12:16 +0000

    Built from commit 3de65dc1734a84ab23295926c5de0c78e269af04

--- themes/ckazu/static/js/application.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    $('.tweet').socialbutton('twitter', {button: 'horizontal'});
-    $('.facebook_like').socialbutton('facebook_like');
-    $('.hatena').socialbutton('hatena');
-    $('.evernote').socialbutton('evernote');
-});
